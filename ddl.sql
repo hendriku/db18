1665a9235b65a3924cd81597040c9efb60c62f05
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-21 15:04:15 +0200

    Document steps

---
+++ ddl.sql
@@ -0,0 +1,8 @@
+/* Wir haben Oracle X11g Express benutzt, da es bereits durch die Vorlesung installiert und eingerichtet war. Daher mussten wir
+nur einen neuen Datenbankbenutzer anlegen und konnten unsere in der Vorlesung bereits unter Beweis gestellten Kenntnisse direkt
+anwenden.*/
+
+-- Create skeleton
+-- Delete everything again
+-- Add constraints
+-- generate random content in insert sql
