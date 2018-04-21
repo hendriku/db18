a6c1ae1fec1dffdbd5eea39dcfa4319e8d2c8ce7
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-21 15:43:44 +0200

    Create tables and kill them slowly

--- ddl.sql
+++ ddl.sql
@@ -2,7 +2,52 @@
 nur einen neuen Datenbankbenutzer anlegen und konnten unsere in der Vorlesung bereits unter Beweis gestellten Kenntnisse direkt
 anwenden.*/
 
--- Create skeleton
--- Delete everything again
+-- Create skeleton (CHECK)
+-- Delete everything again (CHECK)
 -- Add constraints
 -- generate random content in insert sql
+
+CREATE TABLE ZUG(
+    ZUG_ID NUMBER(10),
+    ZUG_TYP VARCHAR(8)
+);
+
+CREATE TABLE MITFAHRER(
+    VERBINDUNGS_ID NUMBER(10),
+    MITARBEITER_ID NUMBER(10)
+);
+
+CREATE TABLE HALTESTELLE(
+    HALTESTELLEN_ID NUMBER(10),
+    HALTESTELLEN_NAME VARCHAR(144),
+    BARRIEREFREI CHARACTER(1)
+);
+
+CREATE TABLE STRECKE(
+    STRECKEN_ID NUMBER(10),
+    HALTESTELLEN_ID NUMBER(10),
+    POS_INDEX NUMBER(10)
+);
+
+CREATE TABLE VERBINDUNG(
+    VERBINDUNGS_ID NUMBER(10),
+    STRECKEN_ID NUMBER(10),
+    POS_INDEX NUMBER(10),
+    ZUG_ID NUMBER(10),
+    ANKUNFTSZEIT DATE,
+    ABFAHRTSZEIT DATE,
+    GLEIS NUMBER(3)
+);
+
+CREATE TABLE MITARBEITER(
+    MITARBEITER_ID NUMBER(10),
+    MITARBEITER_TYP VARCHAR(16),
+    MITARBEITER_VORNAME VARCHAR(32),
+    MITARBEITER_NAME VARCHAR(32)
+);
+
+CREATE TABLE URLAUB(
+    MITARBEITER_ID NUMBER(10),
+    URLAUBSANFANG DATE,
+    URLAUBSENDE DATE
+);
